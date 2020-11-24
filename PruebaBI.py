995a6967d127682b1014d112a322e21572b070bf
020697Dmv <dmaciasv@uqvirtual.edu.co>
2020-11-23 23:59:57 -0500

    Commit

--- PruebaBI.py
+++ PruebaBI.py
@@ -4,17 +4,49 @@ api_key = 'AIzaSyCK2qE6uIz7QLqybqW9SX12r5MokRFkF4M'
 
 youtube = build('youtube', 'v3', developerKey=api_key)
 
-pl_request = youtube.playlistItems().list(
+'''pl_request = youtube.playlistItems().list(
 	part='contentDetails',
-	playlistId="PLw-VjHDlEOgso_-R_g0FkFL1qd5D6vbxD"
+	playlistId="PL1av4CQniLB0dk5LnfWQhBUcRlzo2jBYB",
+	maxResults=50
 
 
 	)
 
 pl_response = pl_request.execute()
 
+
+
 for item in pl_response['items']:
 
 	vid_id=item['contentDetails']['videoId']
 	print(vid_id)
 	print()
+'''
+listas = ['PL1av4CQniLB0dk5LnfWQhBUcRlzo2jBYB','PL952AEBEF137A5446']
+archivos=[]
+pagina=None
+aux=True
+
+while aux==True or pagina!=None:
+	pl_request = youtube.playlistItems().list(
+	part='contentDetails',
+	playlistId="PL952AEBEF137A5446",
+	maxResults=50,pageToken=pagina
+
+
+	)
+
+	aux=False
+	pl_response = pl_request.execute()
+	try:
+		pagina=pl_response['nextPageToken']
+		archivos.extend(pl_response['items'])
+	except Exception as e:
+		pagina=None
+	
+	#print(len(archivos))
+
+
+print(archivos[0]['statistics'])
+
+
