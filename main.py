d59d0493b56e94d8c431c8e660657b00362b251d
020697Dmv <55223897+020697Dmv@users.noreply.github.com>
2020-11-29 23:46:07 -0500

    coomit final

--- main.py
+++
@@ -1,8 +0,0 @@
-from Channels import Ystats
-
-API_KEY="AIzaSyCK2qE6uIz7QLqybqW9SX12r5MokRFkF4M"
-channel_id= "UCOmHUn--16B90oW2L6FRR3A"
-
-chs=Ystats(API_KEY,channel_id)
-chs.get_channel_statistics()
-
